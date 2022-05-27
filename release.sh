4f1183f619503582dd03051b3338d05dfd2eb299
balebom <balebom@gmail.com>
2022-05-27 11:16:19 +0700

    add cpack

---
+++ release.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo -e "Bootstrap application before building"
+if ! ls -AU "./camelServe/external/vcpkg" | read _;
+then
+    git submodule update --init
+fi
+./camelServe/external/vcpkg/bootstrap-vcpkg.sh &
+VCPKG_BOOSTRAP_ID=$!
+wait $VCPKG_BOOSTRAP_ID
+VCPKG_EXIT_CODE=$?
+VCPKG_EXIT_OK=0
+if [ $VCPKG_EXIT_CODE -eq $VCPKG_EXIT_OK ]
+then
+    cmake -S camelServe/ -B build/ -DCMAKE_INSTALL_PREFIX=../camelMap.image/camelServe -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE="Release"
+else
+    echo -e "Bootstrap vcpkg failed, please try later."
+fi
